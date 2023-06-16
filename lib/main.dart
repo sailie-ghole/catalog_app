f6e32b35019488e2eda1a968d32a1d17e14ff6d9
sailieg <gholesailie@gmail.com>
2023-06-17 02:13:58 +0530

    day3 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
+import 'package:catalog_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
 
 void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to Learning Flutter"),
-            ),
-        ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch:Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  
+
+  final int days = 30;
+  final String name = "Sailie";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+        body: Center(
+          child: Container(
+            child: Text("Welcome to Learning Flutter $days : $name"),
+            ),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
